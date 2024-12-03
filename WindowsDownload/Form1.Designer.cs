4c8b47a327c0f21989ef98c7965bfd5b556f7e30
d0m-1k <d0m_1k@mail.ru>
2024-12-03 18:55:50 +0500

    Добавьте файлы проекта.

---
+++ WindowsDownload/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowsDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(184, 323);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(466, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Добро пожаловать в установщик Windows 12\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(351, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Download\r\n";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(280, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(263, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выбирите язык и версию";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Россия",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский",
+            "Русский"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(207, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Visible = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Windows 12 Basik",
+            "Windows 12 Default",
+            "Windows 12 Home",
+            "Windows 12 Pro",
+            "Windows 12 ProPlus",
+            "Windows 12 Premium",
+            "Windows 12 C++",
+            "Windows 12 C#",
+            "Windows 12 Python",
+            "Windows 12 Assembly"});
+            this.comboBox2.Location = new System.Drawing.Point(581, 52);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(207, 24);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsDownload/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsDownload
+{
+
+
+
+
+    public partial class Form1 : Form
+    {
+
+        bool list1 = true;
+        bool list2 = false;
+        bool list3 = false;
+        bool list4 = false;
+        bool list5 = false;
+        bool list6 = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Download()
+        {
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                label2.Text = "Пожалуйста, выбирите значения!";
+
+                label2.Location = new Point(234, 323);
+
+
+                label2.Visible = true;
+                timer1.Enabled = true;
+
+
+            }
+        }
+
+        public void list3Start()
+        {
+
+        }
+
+        public void list2Start() 
+        { 
+
+            if (list1 != true) 
+            {
+                list2 = true;
+
+                comboBox1.Visible = true;
+                comboBox2.Visible = true;
+
+                button1.Visible = true;
+
+                label1.Visible = true;
+            }
+
+            else { list3Start(); }
+
+
+
+
+            
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (list1)
+            {
+                list1 = false;
+
+                button1.Visible = false;
+                label2.Visible = false;
+
+                list2Start();
+
+            }
+            else if (list2)
+            {
+                Download();
+            } 
+
+
+
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            timer1.Enabled = false; 
+        }
+    }
+}
